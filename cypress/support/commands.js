de5402288ea1760347b9740f687f0f4e99bdb69a
epsomsegura <segurajaramilloepsom@gmail.com>
2025-03-31 20:51:34 -0600

    Cypress testing environment hotfix

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,5 +24,12 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 Cypress.Commands.add('resetDatabase', () => {
-  cy.exec('php artisan migrate:fresh --seed --env=testing');
+  cy.exec('php artisan migrate:fresh --seed --env=testing -v', {
+    failOnNonZeroExit: false
+  }).then((result) => {
+    if (result.code !== 0) {
+      cy.log('Migration Error:', result.stderr);
+      throw new Error('Migration failed');
+    }
+  });
 });
