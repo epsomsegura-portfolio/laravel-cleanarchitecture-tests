77f4ea501665d0b769ffd85a693c121a3140a336
epsomsegura <segurajaramilloepsom@gmail.com>
2025-03-31 20:59:26 -0600

    Upgrading cypress auth test

--- cypress/e2e/auth/login.cy.js
+++ cypress/e2e/auth/login.cy.js
@@ -10,10 +10,18 @@ beforeEach(() => {
 
 context("POST /api/auth/login", () => {
   it("Testing api login", () => {
-    cy.request("POST", "/api/auth/login",{
-      "email" : "es_tests_admin@yopmail.com",
-      "password" : "@pA55w0Rd_T3sT",
-      "remember_me" : 1
+    cy.request({
+      method: "POST",
+      url: "/api/auth/login", 
+      body: {
+        "email" : "es_tests_admin@yopmail.com",
+        "password" : "@pA55w0Rd_T3sT",
+        "remember_me" : 1
+      },
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      }
     }).then((response) => {
       expect(response.status).to.eq(200)
       expect(response.body.code).eq('200');
